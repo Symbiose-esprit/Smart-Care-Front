89dce6c9531771975c46ff44d985ab4afe5aa1a3
MaryemBess <47980211+MaryemBess@users.noreply.github.com>
2022-03-23 17:57:22 +0100

    login update

--- pages/features/User/UserSlice.js
+++ pages/features/User/UserSlice.js
@@ -1,19 +1,125 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+export const signupUser = createAsyncThunk(
+  'users/signupUser',
+  async ({ name, email, password }, thunkAPI) => {
+    try {
+      const response = await fetch(
+        'http://localhost:8000/api/v1/signup',
+        {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            name,
+            email,
+            password,
+          }),
+        }
+      );
+      let data = await response.json();
+      console.log('data', data);
+
+      if (response.status === 200) {
+        localStorage.setItem('token', data.token);
+        return { ...data, username: name, email: email };
+      } else {
+        return thunkAPI.rejectWithValue(data);
+      }
+    } catch (e) {
+      console.log('Error', e.response.data);
+      return thunkAPI.rejectWithValue(e.response.data);
+    }
+  }
+);
+
+export const loginUser = createAsyncThunk(
+  'users/login',
+  async ({ email, password }, thunkAPI) => {
+    try {
+      const response = await fetch(
+        'http://localhost:8000/api/v1/signin',
+        {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            email,
+            password,
+          }),
+        }
+      );
+      let data = await response.json();
+      console.log('response', data);
+      if (response.status === 200) {
+        localStorage.setItem('token', data.token);
+        return data;
+      } else {
+        return thunkAPI.rejectWithValue(data);
+      }
+    } catch (e) {
+      console.log('Error', e.response.data);
+      thunkAPI.rejectWithValue(e.response.data);
+    }
+  }
+);
+
+export const fetchUserBytoken = createAsyncThunk(
+  'users/fetchUserByToken',
+  async ({ token }, thunkAPI) => {
+    try {
+      const response = await fetch(
+        'https://mock-user-auth-server.herokuapp.com/api/v1/users',
+        {
+          method: 'GET',
+          headers: {
+            Accept: 'application/json',
+            Authorization: token,
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+      let data = await response.json();
+      console.log('data', data, response.status);
+
+      if (response.status === 200) {
+        return { ...data };
+      } else {
+        return thunkAPI.rejectWithValue(data);
+      }
+    } catch (e) {
+      console.log('Error', e.response.data);
+      return thunkAPI.rejectWithValue(e.response.data);
+    }
+  }
+);
+
 export const userSlice = createSlice({
-  name: "user",
+  name: 'user',
   initialState: {
-    name: "",
-    lastname:"",
-    password:"",
-    email: "",
-    role:"",
+    username: '',
+    password: '',
     isFetching: false,
     isSuccess: false,
     isError: false,
-    errorMessage: "",
+    errorMessage: '',
   },
   reducers: {
+    clearState: (state) => {
+      state.isError = false;
+      state.isSuccess = false;
+      state.isFetching = false;
+
+      return state;
+    },
+  },
+  extraReducers: {
     [signupUser.fulfilled]: (state, { payload }) => {
+      console.log('payload', payload);
       state.isFetching = false;
       state.isSuccess = true;
       state.email = payload.user.email;
@@ -43,89 +149,24 @@ export const userSlice = createSlice({
     [loginUser.pending]: (state) => {
       state.isFetching = true;
     },
-  },
-  extraReducers: {
-    // Extra reducer comes here
+    [fetchUserBytoken.pending]: (state) => {
+      state.isFetching = true;
+    },
+    [fetchUserBytoken.fulfilled]: (state, { payload }) => {
+      state.isFetching = false;
+      state.isSuccess = true;
+
+      state.email = payload.email;
+      state.username = payload.name;
+    },
+    [fetchUserBytoken.rejected]: (state) => {
+      console.log('fetchUserBytoken');
+      state.isFetching = false;
+      state.isError = true;
+    },
   },
 });
-/*
-name: req.body.name,
-    lastname: req.body.lastname,
-    login: req.body.login,
-    password: bcrypt.hashSync(req.body.password, 8),
-    email: req.body.email,
-    role: req.body.role,
-    sex: req.body.sex,
-    address: req.body.address,
-    telephone: req.body.telephone,
-    dateofbirth: req.body.dateofbirth,
-    age: req.body.age,
-*/
-export const signupUser = createAsyncThunk(
-  "users/signupUser",
-  async ({ name, lastname, password, email }, thunkAPI) => {
-    try {
-      const response = await fetch(
-        "http://localhost:8000/api/v1/signup",
-        {
-          method: "POST",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            name,
-            lastname,
-            password,
-            email,
-           
-          }),
-        }
-      );
-      let data = await response.json();
-      console.log("data", data);
-      if (response.status === 200) {
-        localStorage.setItem("token", data.token);
-        return { ...data, username: name, email: email };
-      } else {
-        return thunkAPI.rejectWithValue(data);
-      }
-    } catch (e) {
-      console.log("Error", e.response.data);
-      return thunkAPI.rejectWithValue(e.response.data);
-    }
-  }
-);
-export const loginUser = createAsyncThunk(
-  "users/signin",
-  async ({ email, password }, thunkAPI) => {
-    try {
-      const response = await fetch(
-        "http://localhost:8000/api/v1/signin",
-        {
-          method: "POST",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            email,
-            password,
-          }),
-        }
-      )
-      let data = await response.json()
-      console.log("response", data)
-      if (response.status === 200) {
-        localStorage.setItem("token", data.token)
-        return data
-      } else {
-        return thunkAPI.rejectWithValue(data)
-      }
-    } catch (e) {
-      console.log("Error", e.response.data)
-      thunkAPI.rejectWithValue(e.response.data)
-    }
-  }
-)
+
+export const { clearState } = userSlice.actions;
+
 export const userSelector = (state) => state.user;
---
+++ pages/sign-in.js
@@ -0,0 +1,120 @@
+import React , { Fragment, useEffect } from 'react';
+import TopHeader from '../components/_App/TopHeader';
+import Navbar from '../components/_App/Navbar';
+import PageBanner from '../components/Common/PageBanner';
+import Footer from '../components/_App/Footer';
+import Link from 'next/link';
+import { useSelector, useDispatch } from 'react-redux';
+import { loginUser, userSelector, clearState } from './features/User/UserSlice';
+import { useHistory } from 'react-router-dom';
+
+const SignIn = () => {
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const { register, errors, handleSubmit } = useForm();
+    const { isFetching, isSuccess, isError, errorMessage } = useSelector(
+        userSelector
+    );
+    const onSubmit = (data) => {
+        dispatch(loginUser(data));
+    };
+
+    useEffect(() => {
+        return () => {
+            dispatch(clearState());
+        };
+    }, []);
+
+    useEffect(() => {
+        if (isError) {
+            toast.error(errorMessage);
+            dispatch(clearState());
+        }
+
+        if (isSuccess) {
+            dispatch(clearState());
+            history.push('/');
+        }
+    }, [isError, isSuccess]);
+
+    return (
+        <>
+            <TopHeader />
+
+            <Navbar />
+
+            <PageBanner
+                pageTitle="Sign In"
+                homePageUrl="/"
+                homePageText="Home"
+                activePageText="Sign In"
+                bgImage="page-title-one"
+            />
+
+            <div className="signup-area ptb-100">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-lg-6 pl-0">
+                            <div className="login-left">
+                                <img src="/images/login-bg.jpg" alt="Login" />
+                            </div>
+                        </div>
+
+                        <div className="col-lg-6 ptb-100">
+                            <div className="signup-item">
+                                <div className="signup-head">
+                                    <h2>Login Here</h2>
+                                    <p>Didn't you account yet? <Link href="/sign-up"><a>Sign Up Here</a></Link></p>
+                                </div>
+                                <div className="signup-form">
+                                    <form>
+                                        <div className="row">
+                                            <div className="col-lg-6">
+                                                <div className="form-group">
+                                                    <input type="text" className="form-control" placeholder="First Name" />
+                                                </div>
+                                            </div>
+                                            <div className="col-lg-6">
+                                                <div className="form-group">
+                                                    <input type="email" className="form-control" placeholder="Your Email" />
+                                                </div>
+                                            </div>
+                                            <div className="col-lg-6">
+                                                <div className="form-group">
+                                                    <input type="password" className="form-control" placeholder="Password" />
+                                                </div>
+                                            </div>
+                                            {/* <div className="col-lg-6">
+                                                <div className="form-group">
+                                                    <input type="password" className="form-control" placeholder="Confirm Password" />
+                                                </div>
+                                            </div> */}
+
+                                            <div className="col-lg-12">
+                                                <div className="form-group">
+                                                    <div className="forgot-pass">
+                                                        <a href="#">Forgot Password?</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div className="col-lg-12">
+                                                <div className="text-center">
+                                                    <button type="submit" className="btn signup-btn" onClick={onSubmit}>Login</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <Footer />
+        </>
+    )
+}
+
+export default SignIn;
