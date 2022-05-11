fa5890b1723cb850a8916dabb56e8ab29bd0906b
yesminebaoueb <yasmine.baouab@esprit.tn>
2022-05-11 01:14:03 +0100

    doctors front

--- src/pages/client/doctor/doctors.js
+++ src/pages/client/doctor/doctors.js
@@ -1,34 +1,38 @@
-import Link from 'next/link';
-import React from 'react';
-import { doctorRows } from "../../../dummyData";
-import { useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux';
-import { useEffect, useState } from "react";
-import PageBanner from '../../../components/Client/Common/PageBanner';
-import { DiseaseSelector, predictDisease } from '../../../components/features/User/DiseaseSlice';
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import PageBanner from "../../../components/Client/Common/PageBanner";
 import { ListDoctors } from "../../../components/features/User/AdminSlice";
+import { doctorRows } from "../../../dummyData";
 const Doctors = () => {
-
-    // redux actions
-    const [data, setData] = useState(doctorRows);
+  // redux actions
+  const [data, setData] = useState(doctorRows);
 
   const handleDelete = (id) => {
-        setData(data.filter((item) => item.id !== id));
-    };
-    const dispatch = useDispatch();
+    setData(data.filter((item) => item.id !== id));
+  };
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        const doctors = dispatch(ListDoctors());
-        console.log("doctors",doctors);
-        
+  useEffect(() => {
+    const doctors = dispatch(ListDoctors());
+    console.log("doctors", doctors);
   });
 
-  
-  const onSubmit = (data) => {
-      console.log("doctors",doctors);
-   
-};
-    /*const { } = useSelector(
+  const [doctors, setDoctors] = useState([]);
+
+  const getDoctors = () => {
+    dispatch(ListDoctors()).then((response) => {
+      console.log(response.payload.data.doctor);
+      // doctors = response.payload.data.doctor
+      setDoctors(response.payload.data.doctor);
+    });
+  };
+
+  useEffect(() => {
+    getDoctors();
+  }, []);
+
+  /*const { } = useSelector(
         DiseaseSelector
     );
 
@@ -44,85 +48,85 @@ const Doctors = () => {
         console.log(data);
         dispatch(predictDisease(data));
     };*/
-    return (
-        <>
+  return (
+    <>
+      <PageBanner
+        pageTitle="Meet Our Qualified Doctors"
+        homePageUrl="/"
+        homePageText="Home"
+        activePageText="Doctors"
+        bgImage="page-title-five"
+      />
 
-
-            <PageBanner
-                pageTitle="Meet Our Qualified Doctors"
-                homePageUrl="/"
-                homePageText="Home"
-                activePageText="Doctors"
-                bgImage="page-title-five"
-            />
-
-            <div className="doctor-search-area">
-                <div className="container">
-                    <form>
-                        <div className="row doctor-search-wrap">
-                            <div className="col-sm-6 col-lg-6">
-                                <div className="doctor-search-item">
-                                    <div className="form-group">
-                                        <i className="icofont-doctor-alt"></i>
-                                        <label>Search</label>
-                                        <input type="text" className="form-control" placeholder="Doctor Name" />
-                                    </div>
-                                    <button type="submit" onSubmit={onSubmit()} className="btn doctor-search-btn">
-                                        <i className="icofont-search-1"></i>
-                                    </button>
-                                </div>
-                            </div>
-
-                            <div className="col-sm-6 col-lg-6">
-                                <div className="doctor-search-item">
-                                    <div className="form-group">
-                                        <i className="icofont-hospital"></i>
-                                        <label>Category</label>
-                                        <select className="form-control">
-                                            <option>Neurosurgeon</option>
-                                            <option>Cardiology</option>
-                                            <option>Pathology</option>
-                                            <option>Dental Care</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
+      <div className="doctor-search-area">
+        <div className="container">
+          <form>
+            <div className="row doctor-search-wrap">
+              <div className="col-sm-6 col-lg-6">
+                <div className="doctor-search-item">
+                  <div className="form-group">
+                    <i className="icofont-doctor-alt"></i>
+                    <label>Search</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Doctor Name"
+                    />
+                  </div>
+                  <button type="submit" className="btn doctor-search-btn">
+                    <i className="icofont-search-1"></i>
+                  </button>
                 </div>
-            </div>
+              </div>
 
-            <div className="doctors-area doctors-area-two pt-100 pb-70">
-                <div className="container">
-                    <div className="row justify-content-center">
-                    {/*{data.map(doctors =>
-                        <div className="col-sm-6 col-lg-4">
-                            <div className="doctor-item">
-                                <div className="doctor-top">
-                                    <img src="/images/doctors/doctor1.jpg" alt="Doctor" />
-
-                                    <Link href="/client/appointments/schedule">
-                                        <a>Get Appointment</a>
-                                    </Link>
-                                </div>
-                                <div className="doctor-bottom">
-                                    <h3>
-                                        <Link href="/doctor-details">
-                                            <a>Dr. {dortor.lastname}</a>
-                                        </Link>
-                                    </h3>
-                                    <span>Neurosurgeon</span>
-                                </div>
-                            </div>
-                        </div>
-                    )}  */  }                       
-                    </div>
+              <div className="col-sm-6 col-lg-6">
+                <div className="doctor-search-item">
+                  <div className="form-group">
+                    <i className="icofont-hospital"></i>
+                    <label>Category</label>
+                    <select className="form-control">
+                      <option>Neurosurgeon</option>
+                      <option>Cardiology</option>
+                      <option>Pathology</option>
+                      <option>Dental Care</option>
+                    </select>
+                  </div>
                 </div>
+              </div>
             </div>
+          </form>
+        </div>
+      </div>
 
+      <div className="doctors-area doctors-area-two pt-100 pb-70">
+        <div className="container">
+          <div className="row justify-content-center">
+            {doctors.map((doctor) => (
+              <div className="col-sm-6 col-lg-4">
+                <div className="doctor-item">
+                  <div className="doctor-top">
+                    <img src= {"/images/doctors/" + doctor.img }alt="Doctor" />
 
-        </>
-    )
-}
+                    <Link href="/appointment">
+                      <a>Get Appointment</a>
+                    </Link>
+                  </div>
+                  <div className="doctor-bottom">
+                    <h3>
+                      <Link href="/doctor-details">
+                        <a>Dr. {doctor.lastname}</a>
+                      </Link>
+                    </h3>
+                    <span>{doctor.specialty}</span>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Doctors;
